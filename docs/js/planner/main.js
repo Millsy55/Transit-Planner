dfa04e34146c2a71a6945fc9b32ee32abe7c7414
Millsy55 <adam.millsy@gmail.com>
2022-04-02 12:31:58 -0400

    werkin

---
+++ docs/js/planner/main.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',{
+        attribution: '© OpenStreetMap contributors',
+        maxZoom: 16,
+        minZoom: 8
+}).addTo(map);
+
+var layer_stations = L.featureGroup();
+
+map.addLayer(layer_stations);
