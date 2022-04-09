fd97ecd8280501f4bc703904aec919eeee873ff7
Adam Mills <adam.millsy@gmail.com>
2022-04-09 18:27:14 -0400

    webpack first install

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import leaflet from 'leaflet';
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',{
+        attribution: '© OpenStreetMap contributors',
+        maxZoom: 16,
+        minZoom: 8
+}).addTo(map);
+
+var layer_stations = L.featureGroup();
+
+map.addLayer(layer_stations);
+
+
+function settingsHide(){
+  document.getElementById("settings-main").style.visibility = "hidden";
+}
+
+function settingsShow(){
+  document.getElementById("settings-main").style.visibility = "visible";
+}
+
+function infoShow(){
+  document.getElementById("info-main").style.visibility = "visible";
+}
+
+function infoHide(){
+  document.getElementById("info-main").style.visibility = "hidden";
+}
+
+
+
+ document.body.appendChild(component());
